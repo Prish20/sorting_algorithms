955d08f325fea9467e8ce8922250cbc3440f7f9f
Prish20 <prish@students.uonbi.ac.ke>
2024-01-21 14:14:09 +0000

    Bubble sort fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,46 @@
 #include "sort.h"
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * using the Bubble sort algorithm
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
  *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * Description: Prints the array after each swap.
  */
-
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp;
+	bool swapped;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				swap_ints(array + j, array + j + 1);
 				print_array(array, size);
+				swapped = true;
 			}
 		}
+		if (swapped == false)
+			break;
 	}
 }
--- sort.h
+++ sort.h
@@ -3,6 +3,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -10,6 +13,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
 	const int n;
@@ -19,9 +23,6 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-
 void bubble_sort(int *array, size_t size);
-
-
 
 #endif
